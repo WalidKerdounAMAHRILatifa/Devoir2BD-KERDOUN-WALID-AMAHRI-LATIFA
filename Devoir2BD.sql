4c14178bd8f6c2af3717043b1bbb83165ebca202
WalidKerdounAMAHRILatifa <62473518+WalidKerdounAMAHRILatifa@users.noreply.github.com>
2020-03-24 17:24:52 +0100

    Update Devoir2BD.sql

--- Devoir2BD.sql
+++ Devoir2BD.sql
@@ -27,6 +27,7 @@ END;
 
 -->Question C8
 
+------------>Solution 1 sans création de table ERREUR
 create or replace procedure p2 (num_pilote varchar2) is
     erreur_insr1 EXCEPTION;
     erreur_insr2 EXCEPTION;
@@ -70,6 +71,61 @@ begin
 end;
 
 execute p2('13337');
+
+
+---------------->Solution 2 avec Vréation de table ERREUR
+create table ERREUR (
+nomPilote varchar2(20),
+description varchar2(20));
+
+create or replace procedure p3 (num_pilote varchar2) is
+    erreur_insr1 EXCEPTION;
+    erreur_insr2 EXCEPTION;
+    erreur_insr3 EXCEPTION;
+     
+    CURSOR c1 IS 
+  	SELECT nopilot,sal,comm,nom
+    FROM pilote;
+    
+    vnumpilote pilote.nopilot%type;
+    vsal pilote.sal%type;
+    vcomm pilote.comm%type;
+    vnom pilote.nom%type;
+    
+begin
+    open c1;
+        loop
+            fetch c1 into vnumpilote,vsal,vcomm,vnom;
+            if c1%notfound then
+                begin 
+                    raise erreur_insr2;
+                    exit;
+                end;
+            end if;
+            if(num_pilote = vnumpilote and vcomm > vsal) then 
+                raise erreur_insr3;
+            end if;
+            if(num_pilote = vnumpilote and vcomm < vsal) then 
+                raise erreur_insr1;
+            end if;
+        end loop;
+    close c1;
+    EXCEPTION
+        WHEN erreur_insr1 then
+            insert into ERREUR values (num_pilote,'OK');
+            dbms_output.put_line('Nom Pilote: ' || vnom || ' OK');  
+        WHEN erreur_insr2 then
+            insert into ERREUR values (num_pilote,'PILOTE INCONNUE');
+            dbms_output.put_line('PILOTE INCONNUE');
+        WHEN erreur_insr3 then
+            insert into ERREUR values (num_pilote,'OK');
+            dbms_output.put_line('Nom Pilote: ' || vnom || ' OK');  
+           dbms_output.put_line('Nom Pilote: ' || vnom || ' comm > sal');      
+end;
+
+execute p3('1333');
+
+select * from ERREUR;
 
 --Creation des VUES
 -->Question D1
